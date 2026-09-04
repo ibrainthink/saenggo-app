cca8c8ddcd5eda9af5503d8fa47621f430bc2499
ibrainthink <nabangcat@gmail.com>
2026-09-04 09:28:53 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE='saenggo-progress-v2-17-0';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo.png'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const isPage=event.request.mode==='navigate' || event.request.destination==='document';
+  if(isPage){
+    event.respondWith(fetch(event.request).then(resp=>{
+      const copy=resp.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return resp;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{
+    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return resp;
+  })));
+});
